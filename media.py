23a5d6bef0fb32519c6f1d94fb8ba432617865e0
Eduarda Leal <eduardamedeiros1743@gmail.com>
2025-11-05 11:08:56 -0300

    Atualização do codigo media com status do aluno

--- media.py
+++ media.py
@@ -1,5 +1,14 @@
-aluno = input ("Nome do aluno: ")
+aluno = input ("Digite seu nome: ")
 nota1 = float (input("Digite a primeria nota do aluno: "))
 nota2 = float (input("Digite a segunda nota do aluno: "))
 media = (nota1 + nota2)/2
-print (f"A média do aluno é: {media} ")
+
+
+if media < 7 and media > 4:
+    print (f"A média do aluno {aluno} é: {media}, ele está em recuperação.")
+
+elif media >= 7:
+    print (f"A média do aluno {aluno} é: {media}, ele está aprovado!")
+
+else:
+   print (f"A média do aluno {aluno} é: {media}, ele está reprovado!") 
